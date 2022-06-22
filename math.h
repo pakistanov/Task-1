f42663902a771289ffa165379eaa1ab257c664cf
AIDmitry <golub.plumbum@gmail.com>
2022-06-22 22:31:58 +0300

    With eror

--- math.h
+++ math.h
@@ -5,20 +5,33 @@
 template<typename T>
 class storage
 {
-    std::vector<T> content;
+
+    static std::vector<T> content;
     storage()   {};
     storage(storage const &);
     storage & operator=(storage const &);
 public:
     void addTo(T value){
-        static storage OnlyOneObj;
         content.push_back(value);
     }
+
+
     void deleteFrom(T value){
         for (int i = 0; i < content.size(); i++)
         {
             if (content[i]==value) content.erase(i);
         }
+    }
+
+    storage & get()
+    {
+        return  *this;
+    }
+
+    static storage & Get()
+    {
+        static storage OnlyOneObj;
+        return OnlyOneObj;
     }
 };
 
