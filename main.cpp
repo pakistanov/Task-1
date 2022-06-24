fa74b0ccea01abf75809af491794c8d313612ffe
kistanov <73103895+pakistanov@users.noreply.github.com>
2022-06-24 16:27:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "math.h"
+
 using namespace std;
 
 int main() {
     int a=6;
-    storage<typeof(a)> & obj = storage<typeof(a)>::Get();
-    obj.addTo(a);
+    Storage<int> & st = Storage<int>::Instance();
+
+    for (int i = 0; i < 10; i++) {
+        st.AddTo(i);
+    }
+
+    st.DeleteFrom(3);
+
     return 0;
 }
